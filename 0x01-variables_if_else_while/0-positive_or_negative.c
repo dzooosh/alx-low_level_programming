03da222d815c75b698042ce8e5778dcf66b8c95e
Joshua Alonge <alongejoshua@gmail.com>
2022-04-08 15:17:39 -0700

    ALX task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,9 @@ int main(void)
 		rand(time(0));
 		n = rand() - RAND_MAX / 2;
 		/* your code goes there */
-		if (n > 0)                                                                        
-			        printf("%d is positive\n", n);                                             
-		else if(n < 0)                                                               
-			        printf("%d is negative\n", n);                                             
-		else                                                                              
-			        printf("%d is zero\n", n);                       
+		if (n > 0)                                                                      	printf("%i is positive\n", n);                                             
+		else if(n < 0)                                                              		printf("%i is negative\n", n);                                             
+		else                                                       
+			printf("%i is zero\n", n);                       
 		return (0);
 }
