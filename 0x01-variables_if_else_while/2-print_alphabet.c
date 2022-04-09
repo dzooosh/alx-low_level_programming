2ba523e73b0dc014f1ece1ed6016ca4f7b64b113
Joshua Alonge <alongejoshua@gmail.com>
2022-04-09 13:26:07 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ *
+ *
+ * Return: Always 0 (success/complete)
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
