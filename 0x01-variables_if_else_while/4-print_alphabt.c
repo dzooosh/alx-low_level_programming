43fa06016a52c14e49d9162d48740134be6cbc58
Joshua Alonge <alongejoshua@gmail.com>
2022-04-09 15:52:44 -0700

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all alphabets except q and e
+ *
+ *
+ * Return Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+	putchar('\n');
+
+}
