d197792966609b7f600e6c3d46b58e870d42f66f
Joshua Alonge <alongejoshua@gmail.com>
2022-04-10 16:59:49 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,16 +13,13 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
+		putchar(n);
 		if (n != 57)
 		{
-			putchar(n);
-			putchar(', ');
-		}
-		else
-		{
-			putchar(n);
-			putchar('\n');
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
-}
+
