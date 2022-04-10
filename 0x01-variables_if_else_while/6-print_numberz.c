b5302e2ef2ab6dbe8940f8ae2a42df3f01245f14
Alonge Joshua <alongejoshua@gmail.com>
2022-04-10 16:40:31 +0100

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits numbers of base 10 using putchar
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
