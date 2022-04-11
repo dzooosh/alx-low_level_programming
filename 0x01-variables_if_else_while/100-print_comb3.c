a449309003a5d46c0b1619f6ed34827d29666518
Alonge Joshua <alongejoshua@gmail.com>
2022-04-11 03:11:31 +0100

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,29 +9,24 @@
 
 int main(void)
 {
-	int n = 48;
+        int i;
+        int c;
 
-	while (n < 58)
-	{
-		int s = 48;
-
-		while (s < 58)
-		{
-			if (n != s && n < s)
-			{
-				putchar(n);
-				putchar(s);
-				if (n == 56 && s == 57)
-				{
-					break;
-				}
-				putchar(',');
-				putchar(' ');
-			}
-			s++;
-		}
-		n++;
-	}
-	putchar('\n');
-	return (0);
+        for (i = 48; i < 58; i++)
+        {
+                for (c = 48; c < 58; c++)
+                {
+                        if (i != c && i < c)
+                        {
+                                putchar(i);
+                                putchar(c);
+                                if (i == 56 && c == 57)
+                                        break;
+                                printf(",");
+                                printf(" ");
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
