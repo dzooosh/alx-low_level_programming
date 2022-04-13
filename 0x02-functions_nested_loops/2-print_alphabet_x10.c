8e761edbbb4d904f3ebb55ee09dc088217f192e9
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 07:40:50 -0700

    printing all lowercase alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets
+ * Description: printing all alphabets in lowercase in 10 different lines
+ *
+ * Return: all alphabets 10 rows
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
