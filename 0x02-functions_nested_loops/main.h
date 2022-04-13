71395018f964cd7394dfcf9373dab2b8d9783c0a
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 06:17:30 -0700

    updeating main.h with function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar: prints to the stdout
- * @c - character to print
- * return: success 1
- *
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
