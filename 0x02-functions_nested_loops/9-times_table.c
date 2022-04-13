f1c0d9ee4dfb54e6b462f640d0add8a7b1a0c71c
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 15:46:48 -0700

    displaying 24 hours Jack Bauer

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - printing the 9times table
+ *
+ * Return: 9 times tables
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 9)
+				_putchar(i * j);
+			_putchar(i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
