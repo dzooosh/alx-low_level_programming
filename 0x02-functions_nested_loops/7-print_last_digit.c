037288e3262f7be9ab204cc5fd262fe934c2c7cb
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 10:57:20 -0700

    display last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of any numbet
+ * @c: the inputed value
+ *
+ * Return: The last digit
+ */
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
