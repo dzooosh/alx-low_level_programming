83cd0df6c40ee055d6202748dbe24ad01a630603
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 09:20:29 -0700

    converting given value to an absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of inputed value
+ * @int: inputed int value
+ *
+ * Return: returns absolute number of inputed value
+ */
+
+int _abs(int)
+{
+	int c;
+
+	if (c >= 0)
+		return (c);
+	else
+		return (c / -1);
+}
