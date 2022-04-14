4431f908342c92f2703c2f86a94fdfcf0ca7ed12
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 20:01:14 -0700

    reducing or increasing numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,25 @@
 
 void print_to_98(int n)
 {
+	if (n == 98)
+		printf("%d\n", n)
 	while (n != 98)
 	{
 		if (n > 98)
 		{
-			n--;
-			printf("%d ", n);
+			printf("%d", n++);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
 		}
 		else
 		{
-			n++;
-			printf("%d ", n);
+			printf("%d", n--);
+			if (n != 98)
+				printf(", ");
+			else
+				printf("\n");
 		}
 	}
 }
