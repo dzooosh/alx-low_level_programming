f611df25fa45e8e47b87eb0b63558437495075ff
Joshua Alonge <alongejoshua@gmail.com>
2022-04-14 06:18:17 -0700

    debugging tasks 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Test function for positive or negative
- *
  * Return: 0  
  */
 
@@ -11,6 +10,7 @@ int main(void)
 	int i;
 	
 	i = 0;
-	positive_or_negative(i);
+	positive_or_negative(i)
+		;
 	return (0);
 }
