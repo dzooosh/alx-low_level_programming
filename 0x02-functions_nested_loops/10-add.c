8a7edfa98683df2dc039fa50d1101af6babaa07e
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 19:03:18 -0700

    addition of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - addition of given numbers
+ * @f: first number
+ * @s: second number
+ *
+ * Return: total sum of f and s
+ */
+
+int add(int f, int s)
+{
+	return (f + s);
+}
