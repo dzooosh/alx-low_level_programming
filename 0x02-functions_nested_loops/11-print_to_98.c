bc6e288b90fe04a6b81fa6076303909662a1f581
Joshua Alonge <alongejoshua@gmail.com>
2022-04-13 19:22:30 -0700

    reducing or increasing numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - increase lower or reduce higher numbers to 98
+ * @n: the inputed number
+ *
+ * Return: list of numbers till it reaches 98
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			n++;
+			printf("%d ", n);
+		}
+		else
+		{
+			n--;
+			printf("%d ", n);
+		}
+	}
+}
