ccd2712aeabe1d5cb77ccd0ef102c86c267a44ea
Joshua Alonge <alongejoshua@gmail.com>
2022-05-18 10:07:20 -0700

    returns array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	char *ptr = (char *) malloc(sizeof(c) * size);
 
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
 	return (ptr);
