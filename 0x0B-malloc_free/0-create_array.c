916c74226a0a28c47a09b59ec3a48e7f96e803ad
Joshua Alonge <alongejoshua@gmail.com>
2022-05-19 05:37:44 -0700

    creates array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,20 +6,21 @@
  * @size: amount of times to print c
  * @c: value to be printed based on size
  *
- * Return: NULL if nothing is size = 0
+ * Return: pointer of array of chars
+ *	NULL if nothing is size = 0
  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *cr;
 
 	if (size == 0)
 		return (NULL);
-	char *ptr = malloc(sizeof(c) * size);
-
+	cr = malloc(sizeof(c) * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		ptr[i] = c;
-	return (ptr);
+		cr[i] = c;
+	return (cr);
 }
