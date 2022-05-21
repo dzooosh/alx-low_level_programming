0eab9a6960ceef6f5fc64474b83bdfedb2bfa562
Joshua Alonge <alongejoshua@gmail.com>
2022-05-21 16:43:09 -0700

    add struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates structure for dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
