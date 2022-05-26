c874cb4b9794537bb06f6b13644c7d6e32f65b67
Joshua Alonge <alongejoshua@gmail.com>
2022-05-25 17:46:39 -0700

    prints elements of linked list

---
+++ 0x12-singly_linked_list/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of list_t
+ * @h: singly linked list
+ *
+ * Return: number of elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t phelm;
+
+	phelm = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		phelm++;
+	}
+	return (phelm);
+}
