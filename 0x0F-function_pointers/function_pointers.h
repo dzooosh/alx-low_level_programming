6e6a51acbae1921cc0da82bba14245b2244d8300
Joshua Alonge <alongejoshua@gmail.com>
2022-05-27 18:04:16 -0700

    contains the prototype of functions

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+/**
+ * File: function_pointers.h
+ * Author: Joshua Alonge
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /*FUNCTION_POINTERS*/
