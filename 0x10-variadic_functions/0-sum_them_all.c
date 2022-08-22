400f4696b658d23888658ca460fd2a2f7add0386
Joshua Alonge <alongejoshua@gmail.com>
2022-08-22 02:49:14 -0700

    returns the sum of input values

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -24,5 +24,8 @@ int sum_them_all(const unsigned int n, ...)
 	for (index = 0; index < n; index++)
 		sum += va_arg(nums, int);
 	va_end(nums);
-	return (sum);
+	if (n == 0)
+		return 0;
+	else
+		return (sum);
 }
